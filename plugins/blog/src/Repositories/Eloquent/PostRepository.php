9d44fb662d3d470a74e282c577bf7f66d7c0c0aa
unknown <rama_dona14@yahoo.com>
2017-12-28 00:15:06 +0700

    post repository buat berita popular

--- plugins/blog/src/Repositories/Eloquent/PostRepository.php
+++ plugins/blog/src/Repositories/Eloquent/PostRepository.php
@@ -4,6 +4,8 @@ namespace Botble\Blog\Repositories\Eloquent;
 
 use Botble\Support\Repositories\Eloquent\RepositoriesAbstract;
 use Botble\Blog\Repositories\Interfaces\PostInterface;
+use DB;
+use Carbon\Carbon;
 
 class PostRepository extends RepositoriesAbstract implements PostInterface
 {
@@ -284,7 +286,8 @@ class PostRepository extends RepositoriesAbstract implements PostInterface
      */
     public function getPopularPosts($limit, array $args = [])
     {
-        $data = $this->model->orderBy('posts.views', 'DESC')
+		
+        $data = $this->model->orderBy('posts.created_at', 'DESC')->orderBy('posts.views','DESC')->where('posts.category','17')
             ->select('posts.*')
             ->limit($limit);
         if (!empty(array_get($args, 'where'))) {
